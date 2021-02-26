aa8286741f57569ee3db45e69a99f81eec59034e
FernandoZika <fftbueno@gmail.com>
2021-02-26 19:06:00 -0300

    Apenas um teste

--- projeto01/src/OlaMundo.java
+++ projeto01/src/OlaMundo.java
@@ -3,12 +3,9 @@ import javax.swing.*;
 public class OlaMundo {
 
     public static void main(String[] args) {
- System.out.println("Olá Mundo!");
- System.out.println("Sou o Fernando");
+        System.out.println("Olá Mundo!");
+        System.out.println("Sou o Fernando");
     }
-
-
-
 
 
 }
