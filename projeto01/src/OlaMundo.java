29bb64522c969d8a059d68c334e46c99673604b6
FernandoZika <fftbueno@gmail.com>
2021-02-26 19:57:21 -0300

    ClassJOptionPane

--- projeto01/src/OlaMundo.java
+++ projeto01/src/OlaMundo.java
@@ -3,9 +3,11 @@ import javax.swing.*;
 public class OlaMundo {
 
     public static void main(String[] args) {
-        System.out.println("Olá Mundo!");
-        System.out.println("Sou o Fernando");
-        System.out.println("Teste");
+
+
+        JOptionPane.showMessageDialog(null,"Quem venceu a libertadores?");
+
+
     }
 
 
